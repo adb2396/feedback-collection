36bc0461eec75b7cc6a4daafd6c86e7b8e573ffd
Akshay Bhamare <akshaybhamare10@gmail.com>
2019-12-13 14:57:46 +0530

    setup cookie configuration

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport'); 
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport');
 
-mongoose.connect(keys.mongoURI);
+// connecting mongodb 
+mongoose.connect(keys.mongoURI, {useNewUrlParser: true, useUnifiedTopology: true});
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// By default express don't use cookies, so we use package cookie-session
+// which will set our cookie with encryption provided by us 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        keys: [keys.cookieKey]
+    })
+);
+
+// making sure that passport know to make use of cookie 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// auth routes setup 
 require('./routes/authRoutes')(app);
 
 app.listen(PORT);
