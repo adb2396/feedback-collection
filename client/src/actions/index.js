69bde7d8f4349ee6556362fc82f467f0f44e1057
Akshay Bhamare <akshaybhamare10@gmail.com>
2019-12-17 13:11:33 +0530

    added actions for survey

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 export const fetchUser = () => async dispatch => {
     const res= await axios.get('/api/current_user');
@@ -13,3 +13,16 @@ export const handlePaymentToken = (token) => async dispatch => {
 
     dispatch({ type: FETCH_USER, payload: res.data });
 };
+
+export const submitSurvey = (values, history) => async dispatch => {
+    const res = await axios.post('/api/surveys', values);
+
+    history.push('/surveys');
+    dispatch({ type: FETCH_USER, payload: res.data });
+}
+
+export const fetchSyrveys = () => async dispatch => {
+    const res = await axios.get('/api/surveys');
+
+    dispatch({ type: FETCH_SURVEYS, payload: res.data });
+}
